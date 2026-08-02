7923b12519c673ed915ba4b3fb25479ff237168f
Delia-Dev <almadelia020902@gmail.com>
2026-08-01 20:16:26 -0600

    git actions: Add AWS EC2 deploy, all errors fixed, test 3

--- src/main/java/com/portafolio/zomtg/salesflow/security/controller/HomeController.java
+++ src/main/java/com/portafolio/zomtg/salesflow/security/controller/HomeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/health")
     public String home() {
-        return " Sales Management System API is running \\n!-------------------version 3 revisar pipeline let's go";
+        return " Sales Management System API is running \\n!-------------------version 3 revisar pipeline let's go \\n todo debe funcionar ahora";
     }
 
     @GetMapping("/home")
