58a60f1475148b9ac7725cab1e8c4867fc7cc4bf
Delia-Dev <almadelia020902@gmail.com>
2026-09-16 16:08:14 -0600

    update cors

--- src/main/java/com/portafolio/zomtg/salesflow/security/SecurityConfig.java
+++ src/main/java/com/portafolio/zomtg/salesflow/security/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
         );
 
         configuration.setAllowedMethods(
-                List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                List.of("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH")
         );
 
         configuration.setAllowedHeaders(
